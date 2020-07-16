315cf62e23c5e8ea7909947c86b17a0830c22f20
BlueOwlet <64948785+BlueOwlet@users.noreply.github.com>
2020-07-16 18:16:31 -0500

    version with online whitelist

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import json
 
 SCOPES = [
 
@@ -44,11 +45,12 @@ def login():
     service = build('sheets', 'v4', credentials=creds)
 
 class Group:
-    def __init__(self,teacher,group,level,schedule):
+    def __init__(self,teacher,group,level,schedule,feedback):
         self.teacher=teacher
         self.group=group
         self.level=level
         self.schedule=schedule
+        self.feedback=feedback
         self.students=[]
     def addStudent(self,student):
         self.students.append(student)
@@ -61,8 +63,14 @@ class Message:
         self.recipient=recipient
         self.msg=msg
     def send(self):
-        print('Sending Message to: {}'.format(self.recipient))
-        searchBox=browser.find_element_by_xpath("//div[@class='_3qx7_']")
+        print('Sending Message')
+        try:
+            searchBox=browser.find_element_by_xpath("//div[@class='_3qx7_']")
+        except Exception as e:
+            resetSearchBoxButton=browser.find_element_by_xpath("//button[@class='MfAhJ']")
+            resetSearchBoxButton.click()
+            searchBox=browser.find_element_by_xpath("//div[@class='_3qx7_ _3fVV4']")
+
         searchBox.click()
         searchText=browser.find_element_by_xpath("//div[@class='_3FRCZ copyable-text selectable-text']")
         # searchText.click()
@@ -70,42 +78,45 @@ class Message:
         time.sleep(2)
         recipient=browser.find_element_by_xpath("//span[@title='{}']".format(self.recipient))
         recipient.click()
-        time.sleep(1)
+        time.sleep(2)
         messageBox=browser.find_element_by_xpath("//div[@class='_3uMse']")
         pyperclip.copy(self.msg)
         messageBox.send_keys(Keys.CONTROL+'v')
         time.sleep(1)
         sendButton=browser.find_element_by_xpath("//button[@class='_1U1xa']")
-        sendButton.click()
+        # sendButton.click()
 
 
-def GetData():
+def GetData(spreadsheet_id):
     # Call the Sheets API
-    spreadsheet_id = '1gEsoxidCDUByobm0RU7nThlY_-EODVOTc2A6vT-8Sz8'
     sheet = service.spreadsheets()
     global data
-    range='A:E'
+    range='A:Z'
     result = sheet.values().get(spreadsheetId=spreadsheet_id,range=range).execute()
     data = result.get('values', [])
     i=0
     students=[]
     global teachers
     teachers=[]
+
     try:
         for row in data:
             # print('Checking if row exists')
             if row:
                 # print('Row exists')
-                if row[0] is not '':
+                if 'IAG' in row[1] and row[0] != '':
+
                     teachers.append(row[0])
     except Exception as e:
-        print('Data Collection Done')
+        # print(teachers)
+        print('Data in Array Storing Done')
     return teachers
+
 
 
 def GetUniqueTeacher(teachers):
     allTeachers = teachers
-    # print(set(allTeachers))
+    # input(set(allTeachers))
     global uniqueTeachers
     uniqueTeachers = list(set(allTeachers))
     uniqueTeachers.sort()
@@ -130,7 +141,8 @@ def CreateClasses():
                     level=row[2]
                     student=row[3]
                     schedule=row[4]
-                    groups.append(Group(teacher,group,level,schedule))
+                    feedback=row[5]
+                    groups.append(Group(teacher,group,level,schedule,feedback))
 
                     groups[-1].addStudent(row[3])
 
@@ -142,47 +154,43 @@ def CreateClasses():
         print('Class Creation Complete')
 
 def CreateMessages():
-
+    global whitelistTeachers
     for teacher in uniqueTeachers:
-        if teacher in whitelist:
+        print(teacher)
+        msg='''Good morning {teacher}!
+This is the automated weekly summary of your classes in order to follow up with new material for your groups please. Please let me know if you need the next lesson plan for any of the groups below.
 
-            print('Creating message for: {}'.format(teacher))
-            msg='''Good morning {},
-This is the automated summary of your classes in order to follow up new material for your groups or any situation.
+SUMMARY:'''.format(teacher=teacher)
 
-SUMMARY:
-
-'''.format(teacher)
-
-            teacherGroups = [group for group in groups if group.teacher==teacher]
-            for group in teacherGroups:
-                msg+='''
-
+        teacherGroups = [group for group in groups if group.teacher==teacher]
+        # input(teacherGroups)
+        for group in teacherGroups:
+            msg+='''
 {}
 Students: {}
 Level: {}
 Schedule: {}
+Last Feedback: {}
+'''.format(group.group,group.students,group.level,group.schedule,group.feedback)
 
-'''.format(group.group,group.students,group.level,group.schedule)
-
-            msg+='''Please let me know if you need new material or if any of the schedules are outdated,
-
-Cheers'''
-
-        # print(msg)
-            message=Message(group.teacher,msg)
-            time.sleep(1)
-            if message.recipient in whitelist:
-                print('{} - WHITELISTED'.format(message.recipient))
-                try:
-                    message.send()
-                except Exception as e:
-                    print(e)
-                    print('Failed with teacher: {}'.format(message.recipient))
+        msg+="Please let me know if any of the schedules are outdated,\n All the best,\nInterAct"
+        print(msg)
+        # input(group)
+        # print(group)
+        message=Message(group.teacher,msg)
+        try:
+            if message.recipient not in whitelistTeachers:
+                print('{} - Blacklisted'.format(message.recipient))
+                # input('Blacklisted')
             else:
-                print('{} - BLACKLISTED'.format(message.recipient))
-
-
+                # input('Whitelisted')
+                print('Sending Message')
+                print('{} - Whitelisted'.format(message.recipient))
+                message.send()
+                print('Message Sent!')
+        except Exception as e:
+            print(e)
+            print('Failed with teacher: '.format(group.teacher))
 
 
 
@@ -195,45 +203,82 @@ def StartBrowser():
     global options
     global browser
     options = Options()
-    browser = webdriver.Chrome(driver,options=options)
+    # browser = webdriver.Chrome('/usr/bin/chromedriver',options=options)
+    browser = webdriver.Chrome('chromedriver.exe',options=options)
 
 def WhatsappLogin():
     browser.get('https://web.whatsapp.com/')
     time.sleep(10)
 
+def CheckOS():
+    import platform
+    osName=platform.system()
+    print('OS detected: {}'.format(osName))
+    return osName
+
+def Whitelist():
+    global whitelistTeachers
+    whitelist=True
+    if whitelist == True:
+        with open('whitelistTeachers.json') as jsonFile:
+            whitelistTeachers = json.load(jsonFile)
+            whitelistTeachers = list(whitelistTeachers["whitelist"])
+            print('Whitelist: {}'.format(whitelistTeachers))
+
+
+def GetApproval(spreadsheet_id):
+    # Call the Sheets API
+    global whitelistTeachers
+    whitelistTeachers=[]
+    sheet = service.spreadsheets()
+    global dataHR
+    range='A:Z'
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,range=range).execute()
+    dataHR = result.get('values', [])
+    firstAccess=True
+    for row in dataHR:
+        if row:
+            if firstAccess==True:
+                approvalIndex=row.index('Weekly Whatsapp')
+                teacherIndex=row.index('Teacher')
+                firstAccess=False
+            try:
+                if row[approvalIndex] in ['Yes','yes','y','Communicate','Send','Go']:
+                    whitelistTeachers.append(row[0])
+            except Exception as e:
+                continue
+    print('Whitelist: {}'.format(whitelistTeachers))
 
 
 
 def main():
-    import platform
-    global driver
-    if platform.system() == 'Windows':
+    mode='online'#local or online
+    login()
+    print('Login Successful')
+    osName=CheckOS()
+    if osName == 'Windows':
         driver='chromedriver.exe'
     else:
         driver='/usr/bin/chromedriver'
-    print(platform.system())
-    whitelistConfig=True
-    if whitelistConfig==True:
-        global whitelist
-        import json
-        with open('TeacherWhitelist.json') as json_file:
-            whitelist=json.load(json_file)
-            whitelist=whitelist["whitelist"]
-            print('This is the whitelist: {}'.format(whitelist))
-    print('Dowloading teacher list')
-    print('Logging in and Getting data from Google')
-    login()
-    print('Login Successful')
-    teachers=GetData()
+    print('OS check: ',osName)
+
+    spreadsheet_id='1gEsoxidCDUByobm0RU7nThlY_-EODVOTc2A6vT-8Sz8'
+    teachers=GetData(spreadsheet_id)
     uniqueTeachers=GetUniqueTeacher(teachers)
-    print('Creating Teacher Classes')
-    print('Classes Created Successfully')
+
+    if mode=='online':
+        spreadsheet_id='1ZHi2juyZwzZVd32jUyhe3W0KiVLM7oZEv9rd7v1Q9Wc'
+        GetApproval(spreadsheet_id)
+    else:
+        Whitelist()
+
     print('Creating Messages')
     CreateClasses()
     StartBrowser()
+    print('Browser Started')
     WhatsappLogin()
     CreateMessages()
-
+    # browser.quit()
 
 
 
